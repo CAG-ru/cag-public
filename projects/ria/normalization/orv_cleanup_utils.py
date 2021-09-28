33a765cc9700695f720419d67ed86c74f87cc2e2
zorins <s.zorin@outlook.com>
2021-09-28 08:47:20 +0300

    Edited data generation

--- projects/ria/normalization/orv_cleanup_utils.py
+++ projects/ria/normalization/orv_cleanup_utils.py
@@ -2,75 +2,11 @@
 
 import re
 import string
+import os
+import json
+import pandas as pd
 
-from text_utils.simple_cleanup import simple_clean
-
-number_re = re.compile('^\d+$')
-digit_re = re.compile('\d')
-
-def remove_standard_form(ans):
-    form_chunks = [
-        u'7.1.7.',
-        u'источники данных:',
-        u'(место для текстового описания)',
-        u'иные сведения о расходах (возможных поступлениях) бюджетов бюджетной системы российской федерации:',
-        u'источники данных:',
-        u'информация о программах мониторинга и иных способах (методах) оценки достижения заявленных целей регулирования:',
-        u'описание источников информации для расчета показателей (индикаторов):',
-    ]
-
-    for chunk in form_chunks:
-        ans = ans.replace(chunk, '')
-
-    ans = ans.strip()
-    return ans
-
-def remove_junk(ans):
-    ans = str(ans)
-    ans = simple_clean(ans)
-
-    if not ans:
-        return ''
-
-    ans = remove_standard_form(ans)
-
-    translator = str.maketrans(string.punctuation, ' '*len(string.punctuation))
-    no_punct = ans.translate(translator).strip()
-
-    if len(no_punct) == 0:
-        return ''
-
-    #absence = (
-    #    u'не установлена',
-    #    u'информация отсутствует',
-    #    )
-
-    #if no_punct in absence:
-    #    return ''
-
-    return ans
-
-def normalize_estimation(ans):
-    if not ans:
-        return ''
-
-    ans = remove_junk(ans)
-    
-    translator = str.maketrans(string.punctuation, ' '*len(string.punctuation))
-    no_punct = str(ans).translate(translator).strip()
-
-    absence = (
-        u'отсутствует',
-        u'отсутствуют',
-        u'нет',
-        u'не проводилась'
-    )
-
-    if no_punct.strip() in absence:
-        return ''
-
-    else:
-        return ans
+from collections import Counter
 
 
 def is_empty(text):
@@ -103,7 +39,6 @@ def is_junk(text):
             or is_hyphens(text) or is_underscores(text)
 
 
-# Точка входа дворника
 def clean_up(text):
     text = str(text)
     text = text.strip()
@@ -117,7 +52,79 @@ def clean_up(text):
     text = text.replace('«', '"').replace('»', '"')
     return text
 
+def remove_standard_form(text):
+    if pd.isnull(text) or type(text) == int:
+        return text
+    text = text.replace('(указываются полное и краткое наименования)', '')
+    text = text.replace('(есть/нет)', '')
+    text = text.replace('(место для текстового описания)', '')
+    text = text.replace('(дней с момента принятия проекта нормативного правового акта)', '')
+    return text
+
+# Поиск самых частых значений
+def most_freq(series, num):
+    return sorted(Counter(series).items(), key=lambda item: (-item[1], item[0]))[:num]
+
+# Поиск самых коротких заполнений
+def shortest(series, num):
+    return sorted(Counter(series).items(), key=lambda item: (len(item[0]), item[1]))[:num]
+
+# Добываем топ самых коротких и частых значений
+def get_column_features(df, column, top_freq):
+    cleaned_field = df[column].apply(clean_up)
+    mf = pd.DataFrame(most_freq(cleaned_field, top_freq))
+    sh = pd.DataFrame(shortest(cleaned_field, top_freq))
+    return mf, sh
+
+
+def fill_df_info(all_fields, dataframe, df_name):
+    """
+    Функция принимает датафрейм, сравнивает значение нужных полей
+    с мусорными, проставляет соответствующую оценку
+    и возвращает новый датафрейм
+    """
+    with open('normalization/junk/junk_by_field.json', 'r') as fp:
+        junk_by_field = json.load(fp)
+    
+    if df_name not in all_fields.keys():
+        print('No such table in dataset.')
+        return
+    
+    if 'header: id' in dataframe.columns:
+        id_column = 'header: id'
+    else:
+        id_column = 'id'
+    
+    result_df = pd.DataFrame()
+    for index, row in dataframe.iterrows():
+        result_df.loc[index, id_column] = row[id_column]
+        
+        for short, long in all_fields.get(df_name).items():    
+            value = dataframe.loc[index, long]
+            value = clean_up(value)
+
+            # Проверяем, есть ли текущее вхождение 
+            # в "мусоре" для этого поля
+            junk_set = junk_by_field.get(short)
+            if junk_set is not None and value in junk_set:
+                valid = 0
+            else:        
+                valid = 1
+
+            result_df.loc[index, short] = value
+            result_df.loc[index, str(short + '_valid')] = valid
+    return result_df
+
 
 if __name__ == '__main__':
-    assert not normalize_estimation('-')
-    assert not normalize_estimation('источники данных: отсутствуют  (место для текстового описания)') 
+    # Нам нужно очистить их больше: всякие посторонние символы теперь будут мешать
+    test = pd.Series([
+    '', '\r', 'dfd\n', '\r\n', '\n\r\n', 
+    'u', '.',
+    '-', '- --', 
+    '____', 
+    'nan', 'нет',
+    '(место для текстового описания)',
+    '-(место для текстового описания)'
+    ])
+    test.apply(clean_up)
